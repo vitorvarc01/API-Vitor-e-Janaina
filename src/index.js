e57d475bd06286d3462fbe3bcf478799423fcb52
vitorvarc01 <vitorvarc@gmail.com>
2023-12-18 22:31:58 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(cors())
 app.get('/', async (req, res) => {
     try {
         const nomes = await knex('tabela_nomes')
-        return res.status(200).json(`Anjinha❤️ Eu te Amo!!!`)
+        return res.status(200).json(`Anjinha❤️ Eu te Amo!!! `)
     } catch (error) {
         console.log(error)
         return res.status(500).json(error)
