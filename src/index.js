d1d3153a904f6abc99f13688a945a3452f2d9a5f
vitorvarc01 <vitorvarc@gmail.com>
2023-12-18 22:02:38 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require(`express`)
+const knex = require(`./conexao`)
+const cors = require('cors')
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', async (req, res) => {
+    try {
+        const nomes = await knex('tabela_nomes')
+        return res.status(200).json(nomes)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+})
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT)
